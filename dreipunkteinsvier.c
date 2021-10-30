8aaf08c9832f8d2a620bc21965368245265e79bf
dreipunkteinsvier <dreipunkteinsvier@gmail.com>
2021-10-30 22:29:10 +0200

    added caps lock key and caps word function

--- dreipunkteinsvier.c
+++ dreipunkteinsvier.c
@@ -15,3 +15,33 @@
  */
 
 #include "dreipunkteinsvier.h"
+
+// Toggle caps lock following a word
+static void process_caps_word(uint16_t keycode, keyrecord_t *record) {
+	// Get base key code from mod tap
+	if (record->tap.count &&
+		((QK_MOD_TAP < keycode && keycode < QK_MOD_TAP_MAX) ||
+		(QK_LAYER_TAP < keycode && keycode < QK_LAYER_TAP_MAX))
+	) { keycode &= 0x00FF; }
+
+	// Deactivate caps lock with listed keycodes
+	switch (keycode) {
+		case KC_TAB:
+		case KC_ESC:
+		case KC_SPC:
+		case KC_ENT:
+		case KC_DOT:
+		case KC_EQL:
+		case KC_GESC:
+			tap_code(KC_CAPS);
+	}
+}
+
+bool process_record_user(uint16_t const keycode, keyrecord_t *record) {
+    
+	if (record->event.pressed) {
+		if (host_keyboard_led_state().caps_lock) { process_caps_word(keycode, record); }
+	}
+
+	return true; // Continue with unmatched keycodes
+}
